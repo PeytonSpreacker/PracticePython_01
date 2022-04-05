61a9618e5513ddc58ca8a69d8778c2cc8759fb07
PeytonSpreacker <peyton@spreacker.com>
2022-04-05 13:12:55 -0500

    input instructions

---
+++ characterinput.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+print('Your name is '+ name)
+
+age = int(input('What age are you? '))
+print('You will turn 100 years old in', (2022+(100-age)))
+
+copy = int(input('Give me a number between 1 and 10:'))
+print('Give me a number between 1 and 10\n'*copy)
